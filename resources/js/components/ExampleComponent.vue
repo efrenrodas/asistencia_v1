37dac59d6c14ec155a8b8285b869aa546018379c
efrenHouse <efrenrodas1@gmail.com>
2022-01-22 16:33:05 -0500

    version con vue

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                       soy un componente de ejemplo a pasar los datos
                     </div>
                 </div>
             </div>
